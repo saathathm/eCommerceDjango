0b86e28bb2c34d2e73e4f7632cb3e3f2802f74ca
saathathm <saaadhath@gmail.com>
2024-09-08 20:40:31 +0530

    backup

--- saadhath_project/settings.py
+++ saadhath_project/settings.py
@@ -122,9 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = '/images/'
-
-MEDIA_ROUTE = BASE_DIR/'static'
+# MEDIA_URL='/images/'
+MEDIA_ROUTE=BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
